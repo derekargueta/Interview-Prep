9da0e870598007c86f8777aff094ac3818890379
Derek Argueta <darguetap@gmail.com>
2015-11-16 22:54:42 -0700

    Add binary tree

--- src/com/dargueta/trees/BinaryTree.java
+++ src/com/dargueta/trees/BinaryTree.java
@@ -3,5 +3,84 @@ package com.dargueta.trees;
 /**
  * Created by derek on 9/14/15.
  */
-public class BinaryTree {
+public class BinaryTree<T extends Comparable<T>> {
+    private BNode<T> root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public BinaryTree(T t) {
+        this.root = new BNode<T>(t);
+    }
+
+    private boolean innerInsert(T t, BNode<T> node) {
+        int compare = t.compareTo(node.getValue());
+        if(compare < 0) {
+            if(node.hasLeftLeaf()) {
+                return innerInsert(t, node.getLeftLeaf());
+            } else {
+                node.setLeftLeaf(new BNode(t));
+                return true;
+            }
+        } else if(compare > 0) {
+            if(node.hasRightLeaf()) {
+                return innerInsert(t, node.getRightLeaf());
+            } else {
+                node.setRightLeaf(new BNode(t));
+                return true;
+            }
+        }
+
+        // duplicate value
+        return false;
+    }
+
+    public void insert(T t) {
+        if(this.root == null) {
+            this.root = new BNode<T>(t);
+        } else {
+            innerInsert(t, this.root);
+        }
+    }
+}
+
+class BNode<T> {
+    private T t;
+    private BNode leftLeaf;
+    private BNode rightLeaf;
+
+    public BNode(T t) {
+        this.t = t;
+        this.leftLeaf = null;
+        this.rightLeaf = null;
+    }
+
+    public T getValue() {
+        return this.t;
+    }
+
+    public BNode getLeftLeaf() {
+        return this.leftLeaf;
+    }
+
+    public BNode getRightLeaf() {
+        return this.rightLeaf;
+    }
+
+    public void setLeftLeaf(BNode n) {
+        this.leftLeaf = n;
+    }
+
+    public void setRightLeaf(BNode n) {
+        this.rightLeaf = n;
+    }
+
+    public boolean hasLeftLeaf() {
+        return this.leftLeaf != null;
+    }
+
+    public boolean hasRightLeaf() {
+        return this.rightLeaf != null;
+    }
 }
